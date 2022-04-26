90142370f68f1f42d41ab60eafc235e2526f9dab
HimanshuMotiani <himanshu.motiani93@gmail.com>
2022-04-26 20:29:31 +0530

    redux-javascript

---
+++ index.js
@@ -0,0 +1,67 @@
+let h2 = document.querySelector('h2');
+let increment = document.querySelector('.increment');
+let decrement = document.querySelector('.decrement');
+let reset = document.querySelector('.reset');
+
+let store = createStore(reducer)
+counter = store.getState() && store.getState().value ? store.getState().value : 0;
+
+h2.innerText = counter;
+
+increment.addEventListener('click', () => {
+    store.dispatch('increment');
+  });
+  
+  decrement.addEventListener('click', () => {
+    store.dispatch('decrement');
+  });
+  
+  reset.addEventListener('click', () => {
+    store.dispatch('reset');
+  });
+
+  store.subscribe(() => {
+    counter = store.getState().value;
+    h2.innerText = counter;
+  });
+
+function createStore(reducer) {
+    let state;
+    let listeners = [];
+    const getState = () => state;
+    const dispatch = (action) => {
+      state = reducer(state, action);
+      listeners.forEach((listener) => listener());
+    };
+  
+    const subscribe = (listener) => {
+      listeners.push(listener);
+      return () => {
+        listeners.filter((l) => l !== listeners);
+      };
+    };
+    dispatch({});
+    return { getState, dispatch, subscribe };
+  }
+
+  function reducer(state = { value: 0, step: 5 }, action) {
+    switch (action) {
+      case 'increment':
+        return { ...state, value: state.value + state.step };
+  
+      case 'decrement':
+        return { ...state, value: state.value - state.step };
+  
+      case 'reset':
+        return { ...state, value: 0 };
+  
+      case 'incrementByFive':
+        return { ...state, step: 5 };
+  
+      case 'incrementByTen':
+        return { ...state, step: 10 };
+  
+      case 'incrementByFifteen':
+        return { ...state, step: 15 };
+    }
+  }
